2b42d443e734aa8208c4bfb30f9835599c2e729d
xXchelikXx <162018732+xXchelikXx@users.noreply.github.com>
2024-09-29 14:57:55 +0300

    Улучшил send_photo_in_tg.py

--- send_photo_in_tg.py
+++ send_photo_in_tg.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     token = os.environ['TG_BOT_TOKEN']
+    chat_id = os.environ['TG_CHAT_ID']
     bot = telegram.Bot(token=token)
     images = os.walk('images')
     for image in images:
@@ -17,7 +18,7 @@ def main():
         random.shuffle(name_images)
         for name_image in name_images:
             with open(f'images/{download_image}', 'rb') as file:
-                bot.send_photo(chat_id='@beta_bot_test', photo=file)
+                bot.send_photo(chat_id=chat_id, photo=file)
             time.sleep(time_waiting)
 
 
